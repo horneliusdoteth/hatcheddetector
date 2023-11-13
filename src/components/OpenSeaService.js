ec80729f987f509d7c6ac2179c88a4bd1d61b43d
slogansUW <slogans@uw.edu>
2023-11-13 00:14:22 -0800

    obscured api call with lambda function and api gateway

--- src/components/OpenSeaService.js
+++ src/components/OpenSeaService.js
@@ -1,17 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
 
-const apiKey = process.env.REACT_APP_OPENSEA_API_KEY;
-
-export const fetchFloorPrice = async (collectionSlug) => {
-    try {
-        const response = await axios.get(`https://api.opensea.io/api/v1/collection/${collectionSlug}/stats`, {
-          headers: {
-            'X-API-KEY': apiKey
-          }
-        });
-        return response.data.stats.floor_price;
-      } catch (error) {
-        console.error('Error fetching floor price:', error);
-        return null;
-      }
+export const fetchFloorPrice = async (
+  collectionSlug
+) => {
+  try {
+    const response = await axios.get(
+      `https://jxl9d8011k.execute-api.us-east-1.amazonaws.com/floor-price/${collectionSlug}`
+    );
+    console.log("API response:", response.data);
+    return response.data.floorPrice;
+  } catch (error) {
+    console.error(
+      "Error fetching floor price:",
+      error
+    );
+    return null;
+  }
 };
