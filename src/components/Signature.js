4672be2f2be27cb81e23343fef21e8d723612fbd
horneliusdoteth <horn3lius@gmail.com>
2024-12-25 16:38:21 -0800

    fixed broken omnia pet floor price fetch

--- src/components/Signature.js
+++ src/components/Signature.js
@@ -1,40 +1,52 @@
-import React, { useState } from "react";
-
+import React, { useState } from 'react';
 
 const Signature = () => {
-  const [copySuccess, setCopySuccess] = useState('');
+    const [copySuccess, setCopySuccess] = useState('');
 
-  const ethAddress = "0x8D59240a9EF010cc7D1b5671CA4878C659F8D290";
+    const ethAddress = '0x8D59240a9EF010cc7D1b5671CA4878C659F8D290';
 
-  const copyToClipboard = () => {
-    navigator.clipboard.writeText(ethAddress);
-    setCopySuccess("hornelius.eth's Ethereum address was copied to clipboard!");
-    setTimeout(() => setCopySuccess(''), 2000);
-  };
-  return (
-    <div className="signature">
-      <img
-        src="./images/hornelius.jpg"
-        alt="hornelius.eth"
-        style={{
-          width: "75px",
-          height: "75px",
-          position: "relative",
-          borderRadius: "100px",
-        }}
-      />
-      <p className="signature-text">
-        Built by hornelius.eth<br></br><a className="sealtoshis" href="https://x.com/sealtoshis" target="/blank">@sealtoshis</a> launching soon, check it.
-      </p>
-      <div className="ethereum-tip" onClick={copyToClipboard} style={{ cursor: 'pointer' }}>
-          <img className="eth-image" src='./images/ethereum-eth-logo-colored.svg' alt="Ethereum" style={{ width: '20px', height: '20px' }} />
-        <span className="tip-text">
-        Appreciate the magic? Keep me building: {ethAddress}
-        {copySuccess && <div style={{ color: 'green' }}>{copySuccess}</div>}
-        </span>
-      </div>
-    </div>
-  );
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(ethAddress);
+        setCopySuccess(
+            "hornelius.eth's Ethereum address was copied to clipboard!"
+        );
+        setTimeout(() => setCopySuccess(''), 2000);
+    };
+    return (
+        <div className="signature">
+            <img
+                src="./images/hornelius.jpg"
+                alt="hornelius.eth"
+                style={{
+                    width: '75px',
+                    height: '75px',
+                    position: 'relative',
+                    borderRadius: '100px',
+                }}
+            />
+            <p className="signature-text">
+                Built by hornelius.eth<br></br>
+            </p>
+            <div
+                className="ethereum-tip"
+                onClick={copyToClipboard}
+                style={{ cursor: 'pointer' }}
+            >
+                <img
+                    className="eth-image"
+                    src="./images/ethereum-eth-logo-colored.svg"
+                    alt="Ethereum"
+                    style={{ width: '20px', height: '20px' }}
+                />
+                <span className="tip-text">
+                    Appreciate the magic? Keep me building: {ethAddress}
+                    {copySuccess && (
+                        <div style={{ color: 'green' }}>{copySuccess}</div>
+                    )}
+                </span>
+            </div>
+        </div>
+    );
 };
 
 export default Signature;
--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,119 +1,112 @@
-import React, {
-  useState,
-  useEffect,
-} from "react";
-import { fetchFloorPrice } from "./ApiService";
+import React, { useState, useEffect } from 'react';
+import { fetchFloorPrice } from './ApiService';
 
 const PIXLTicker = () => {
-  const [priceInfo, setPriceInfo] = useState({
-    usd: "Loading...",
-    native: "Loading...",
-  });
-  const [sappySealsFloor, setSappySealsFloor] = useState("Loading...");
-  const [genesisPixlPetFloor, setGenesisPixlPetFloor] = useState("Loading...");
+    const [priceInfo, setPriceInfo] = useState({
+        usd: 'Loading...',
+        native: 'Loading...',
+    });
+    const [sappySealsFloor, setSappySealsFloor] = useState('Loading...');
+    const [genesisPixlPetFloor, setGenesisPixlPetFloor] =
+        useState('Loading...');
 
-  useEffect(() => {
-    const fetchPrice = async () => {
-      try {
-        const response = await fetch(
-          "https://api.dexscreener.com/latest/dex/tokens/0x427A03Fb96D9A94A6727fbcfbBA143444090dd64"
-        );
-        const data = await response.json();
-        const firstPair = data.pairs[0];
-        const usdPrice = firstPair.priceUsd;
-        const nativePrice = firstPair.priceNative;
-        setPriceInfo({
-          usd: usdPrice,
-          native: nativePrice,
-        });
-      } catch (error) {
-        console.error(
-          "Error fetching price:",
-          error
-        );
-        setPriceInfo({
-          usd: " N/A",
-          native: " N/A",
-        });
-      }
-    };
-    fetchPrice();
+    useEffect(() => {
+        const fetchPrice = async () => {
+            try {
+                const response = await fetch(
+                    'https://api.dexscreener.com/latest/dex/tokens/0x427A03Fb96D9A94A6727fbcfbBA143444090dd64'
+                );
+                const data = await response.json();
+                const firstPair = data.pairs[0];
+                const usdPrice = firstPair.priceUsd;
+                const nativePrice = firstPair.priceNative;
+                setPriceInfo({
+                    usd: usdPrice,
+                    native: nativePrice,
+                });
+            } catch (error) {
+                console.error('Error fetching price:', error);
+                setPriceInfo({
+                    usd: ' N/A',
+                    native: ' N/A',
+                });
+            }
+        };
+        fetchPrice();
 
-    const fetchFloors = async () => {
-      const sappySealsData = await fetchFloorPrice("sappy-seals");
-      if (sappySealsData === null) {
-        setSappySealsFloor(" N/A");
-      } else {
-        setSappySealsFloor(sappySealsData.floorPrice);
-      }
-    
-      const genesisPixlData = await fetchFloorPrice("pixl-pets-genesis");
-      if (genesisPixlData === null) {
-        setGenesisPixlPetFloor(" N/A");
-      } else {
-        setGenesisPixlPetFloor(genesisPixlData.floorPrice);
-      }
-    };
-    
+        const fetchFloors = async () => {
+            const sappySealsData = await fetchFloorPrice('sappy-seals');
+            if (sappySealsData === null) {
+                setSappySealsFloor(' N/A');
+            } else {
+                setSappySealsFloor(sappySealsData.floorPrice);
+            }
 
-    fetchFloors();
-  }, []);
+            const genesisPixlData = await fetchFloorPrice('omnia-pets-genesis');
+            if (genesisPixlData === null) {
+                setGenesisPixlPetFloor(' N/A');
+            } else {
+                setGenesisPixlPetFloor(genesisPixlData.floorPrice);
+            }
+        };
 
-  const pixlTokenSymbol = "./images/PIXL.png";
+        fetchFloors();
+    }, []);
 
-  return (
-    <div className="pixl-ticker">
-      <div className="ticker-wrap">
-        <div className="ticker-move">
-          <div className="ticker-item">
-            <img
-              src={"./images/omnis_green.png"}
-              alt="Omni Pets"
-              height="30"
-              style={{
-                position: "relative",
-                top: "5px",
-                marginRight: "5px",
-                borderRadius: "100px",
-              }}
-            />
-            Omnis Ξ{genesisPixlPetFloor}
-          </div>
-          <div className="ticker-item">
-            <img
-              src={"./images/sappyseals.jpg"}
-              alt="Sappy Seals"
-              height="30"
-              style={{
-                position: "relative",
-                top: "5px",
-                marginLeft: "25px",
-                marginRight: "5px",
-                borderRadius: "5px",
-              }}
-            />
-            Sappy Seals Ξ{sappySealsFloor}
-          </div>
-          <div className="ticker-item">
-            <img
-              className="ticker-img"
-              src={pixlTokenSymbol}
-              alt="PIXL Token"
-              height="30"
-              style={{
-                position: "relative",
-                top: "4px",
-                marginLeft: "25px",
-                marginRight: "5px",
-              }}
-            />
-            {`PIXL/USD $${priceInfo.usd} PIXL/ETH Ξ${priceInfo.native}`}
-          </div>
+    const pixlTokenSymbol = './images/PIXL.png';
+
+    return (
+        <div className="pixl-ticker">
+            <div className="ticker-wrap">
+                <div className="ticker-move">
+                    <div className="ticker-item">
+                        <img
+                            src={'./images/omnis_green.png'}
+                            alt="Omni Pets"
+                            height="30"
+                            style={{
+                                position: 'relative',
+                                top: '5px',
+                                marginRight: '5px',
+                                borderRadius: '100px',
+                            }}
+                        />
+                        Omnis Ξ{genesisPixlPetFloor}
+                    </div>
+                    <div className="ticker-item">
+                        <img
+                            src={'./images/sappyseals.jpg'}
+                            alt="Sappy Seals"
+                            height="30"
+                            style={{
+                                position: 'relative',
+                                top: '5px',
+                                marginLeft: '25px',
+                                marginRight: '5px',
+                                borderRadius: '5px',
+                            }}
+                        />
+                        Sappy Seals Ξ{sappySealsFloor}
+                    </div>
+                    <div className="ticker-item">
+                        <img
+                            className="ticker-img"
+                            src={pixlTokenSymbol}
+                            alt="PIXL Token"
+                            height="30"
+                            style={{
+                                position: 'relative',
+                                top: '4px',
+                                marginLeft: '25px',
+                                marginRight: '5px',
+                            }}
+                        />
+                        {`PIXL/USD $${priceInfo.usd} PIXL/ETH Ξ${priceInfo.native}`}
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-    
-  );
+    );
 };
 
 export default PIXLTicker;
